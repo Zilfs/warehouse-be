23d16694d9b723509a15aab1dadf50050c82fe95
Zilfs <rheinlandcast@gmail.com>
2026-03-30 20:54:15 +0700

    feat: implement config set up in app

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,22 @@
 package app
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"log"
+	"warehouse/config"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func RunServer() {
+	cfg := config.NewConfig()
+	db, err := cfg.ConnetionPostgres()
+	if err != nil {
+		log.Fatal("Failed to connect to database: %v", err)
+		return
+	}
+
+	_ = db
+
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
